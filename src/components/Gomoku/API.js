93daae61e2a7ca3a14cc6e85ecc5a5ab2332cd3a
ms0223900 <ms0223900@gmail.com>
2019-08-12 17:20:39 +0800

    190812_update storybook api url

--- src/components/Gomoku/API.js
+++ src/components/Gomoku/API.js
@@ -1,7 +1,7 @@
 import openSocket from 'socket.io-client'
 import Strapi from 'strapi-sdk-javascript/build/main'
 
-const apiUrl = process.env.API_URL || 'http://localhost:1337'
+const apiUrl = process.env.STORYBOOK_API || 'http://localhost:1337'
 
 export const socket = openSocket(apiUrl)
 export const strapi = new Strapi(apiUrl)

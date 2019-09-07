62a19b12689ad7709159c100a2d9684edf1dd7b3
ms0223900 <ms0223900@gmail.com>
2019-09-08 00:11:12 +0800

    190908_update check jewel function

--- src/components/HexaNumberSnake/jewelSquare.js
+++ src/components/HexaNumberSnake/jewelSquare.js
@@ -1,38 +1,140 @@
-import React, { useRef, useState, useCallback, forwardRef, useImperativeHandle } from 'react'
-import { Box } from '@material-ui/core'
+import React, { useRef, useState, useCallback, forwardRef, useImperativeHandle, useEffect } from 'react'
+import { Box, makeStyles } from '@material-ui/core'
+
+const jewelWidth = 50
+const jewelsPerRow = 5
 
 
+// const jewelsData_init = [
+//   { id: 0, color: '#f00', },
+//   { id: 1, color: '#00f', },
+//   { id: 2, color: '#0fa', },
+//   { id: 3, color: '#f00', },
+//   { id: 4, color: '#00f', },
+//   { id: 5, color: '#0fa', },
+// ]
 
-const SingleJewel = ({ color, getPosFn }) => {
-  const [offsetPosTop, setTop] = useState(0)
-  const jewelRef = useRef()
-  const getPos = useCallback(() => {
-    console.log(jewelRef.current.offsetTop)
-    getPosFn(jewelRef.current.offsetTop)
-    offsetPosTop === 0 ? setTop(-20) : setTop(0)
-  }, [offsetPosTop, getPosFn])
+const jewelsData_init = [
+  { id: 0, color: '#0fafad', },
+  { id: 1, color: '#0fafad', },
+  { id: 2, color: '#f00', },
+  { id: 3, color: '#0fafad', },
+  { id: 4, color: '#00f', },
+  { id: 5, color: '#0fa', },
+]
+
+const getJewelPos = (id, width) => ({
+  left: ~~(id % jewelsPerRow) * width,
+  top: ~~(id / jewelsPerRow) * width
+})
+
+const useStyles = makeStyles({
+  jewelContainer: {
+    position: 'relative',
+  },
+  singleJewel: {
+    backgroundColor: props => props.color, 
+    position: 'absolute',
+    top: props => props.pos.top,
+    left: props => props.pos.left,
+    width: jewelWidth, 
+    height: jewelWidth,
+    borderRadius: 10,
+    boxSizing: 'border-box',
+    // transform: props => `translate(${props.pos.left}px, ${props.pos.top}px)`,
+    transition: '0.3s'
+  }
+})
+
+const SingleJewel = ({ pos, color, getIdFn }) => {
+  const classes = useStyles({ color, pos })
   return (
-    <Box ref={ jewelRef } onClick={ getPos } style={{ backgroundColor: color, width: 50, height: 50, position: 'relative', top: offsetPosTop, transition: '0.3s' }}></Box>
+    <Box onClick={ getIdFn } className={ classes.singleJewel }></Box>
   )
 }
 
-const jewelsData = [
-  { id: 0 ,color: '#f00', },
-  { id: 1 ,color: '#00f', },
-  { id: 2 ,color: '#0fa', },
-]
-
 const Jewels = () => {
-  const [twoJewelsPos, settwoJewelsPos] = useState(1000)
-  const handleGetPos = useCallback(newPos => {
-    console.log(twoJewelsPos, newPos)
-    settwoJewelsPos(20)
-  }, [twoJewelsPos])
-  console.log(twoJewelsPos)
+  const [jewelData, setJewelData] = useState(jewelsData_init)
+  const [twoJewelsIndex, settwoJewelsIndex] = useState([])
+  const getJewelId = useCallback((newIndex) => {
+    const checkNotSame = !twoJewelsIndex.find(idx => idx === newIndex)
+    const checkIs4Dir = () => {
+      const prevIndex = twoJewelsIndex[0]
+      const lastIndexPerRow = jewelsPerRow - 1
+      if(newIndex + jewelsPerRow === prevIndex || newIndex - jewelsPerRow === prevIndex) {
+        return true
+      } else if((prevIndex % jewelsPerRow !== 0 && prevIndex % jewelsPerRow !== lastIndexPerRow) && (prevIndex + 1 === newIndex || prevIndex - 1 === newIndex)) {
+        return true
+      } else if(prevIndex % jewelsPerRow === 0 && prevIndex + 1 === newIndex) {
+        return true
+      } else if(prevIndex % jewelsPerRow === lastIndexPerRow && prevIndex - 1 === newIndex) {
+        return true
+      }
+      return false 
+    }
+    if(twoJewelsIndex.length < 1) {
+      settwoJewelsIndex([newIndex])
+    } else if( checkNotSame && checkIs4Dir() ) {
+      settwoJewelsIndex(index => [
+        ...index,
+        newIndex
+      ])
+    }
+  }, [twoJewelsIndex])
+  //handle exchange jewels
+  useEffect(() => {
+    if(twoJewelsIndex.length === 2) {
+      console.log(twoJewelsIndex.length)
+      const [index1, index2] = twoJewelsIndex
+      let newJewelData = [...jewelData]
+      const jewel1 = jewelData[index1]
+      const jewel2 = jewelData[index2]
+      newJewelData[index1] = jewel2
+      newJewelData[index2] = jewel1
+      setJewelData(newJewelData)
+      console.log(newJewelData)
+      return settwoJewelsIndex([])
+    }
+  }, [twoJewelsIndex])
+  //handle check is fulfill 3 disappear condition
+  useEffect(() => {
+    const checkIsFulfill = (index, jewelAddCount) => {
+      let sameColorCount = 1
+      let jewelIndex = index
+      let nextIndex = jewelIndex + jewelAddCount
+      const { color } = jewelData[index]
+      console.log(jewelIndex + jewelAddCount)
+      while(jewelData[nextIndex] && jewelData[nextIndex].color === color) {
+        sameColorCount += 1
+        nextIndex += jewelAddCount
+        console.log('hi')
+      }
+      if(sameColorCount > 2) {
+        window.alert('yes!')
+        return true
+      }
+      return false
+    }
+    for (let i = 0; i < jewelData.length; i++) {
+      const columnRes = checkIsFulfill(i, jewelsPerRow)
+      //row(expect last 2 jewels)
+      let rowRes
+      if(i % 5 !== 3 && i % 5 !== 4) {
+        rowRes = checkIsFulfill(i, 1) 
+      }
+      if(columnRes || rowRes) {
+        break
+      }
+    }
+  }, [jewelData])
   return (
     <Box>
-      {jewelsData.map(data => (
-        <SingleJewel key={ data.id } {...data} getPosFn={ handleGetPos } />
+      {jewelData.map((data, index) => (
+        <SingleJewel 
+          key={ data.id }
+          {...data} 
+          pos={ getJewelPos(index, jewelWidth) } 
+          getIdFn={ () => { getJewelId(index) } } />
       ))}
     </Box>
   )
